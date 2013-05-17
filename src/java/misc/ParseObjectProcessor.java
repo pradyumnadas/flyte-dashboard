9675e3712aaf880518e19dae422259c2d4b28fcd
pradyumnadas <pradyumnadas@flipkart-Latitude-E6420>
2013-05-17 15:45:29 +0530

    ParseObjectProcessor now returns ArrayList

--- src/java/misc/ParseObjectProcessor.java
+++ src/java/misc/ParseObjectProcessor.java
@@ -5,11 +5,15 @@
 package misc;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -19,24 +23,28 @@ import java.util.zip.ZipInputStream;
  */
 public class ParseObjectProcessor {
 
-    public static void processObject(ParseObject targetObj) {
-        String[] processedOutput = new String[3];
+    public static ArrayList<File> processObject(ParseObject targetObj) {
+        ArrayList<File> processedOutput = new ArrayList<File>();
         if (targetObj.getType() == ParseObject.ParseType.CRASHDUMP) {
-            String stacktrace = generateStacktrace(targetObj);
+            processedOutput.add(generateStacktrace(targetObj));
         } else if (targetObj.getType() == ParseObject.ParseType.BUG) {
-            String[] info = extractBugInfo(targetObj);
+            for(File f : extractBugInfo(targetObj)) {
+                processedOutput.add(f);
+            }
         } else if (targetObj.getType() == ParseObject.ParseType.FEEDBACK) {
-            String feedbackDesc = getDescription(targetObj);
+            processedOutput.add(getDescription(targetObj));
         } else {
-            String queryDesc = getDescription(targetObj);
+            processedOutput.add(getDescription(targetObj));
         }
+        return processedOutput;
     }
 
-    private static String generateStacktrace(ParseObject targetObj) {
-        String stacktrace = "";
+    private static File generateStacktrace(ParseObject targetObj) {
+        File stacktraceFile;
         Process minidump_stacktwalk;
         String[] cmdArray = new String[3];
         BufferedReader stacktraceReader;
+        BufferedWriter stacktraceWriter;
 
         cmdArray[0] = "/home/pradyumnadas/google-breakpad/src/processor/minidump_stackwalk";
         cmdArray[1] = Settings.getInstance().getParseFileDownloadDirectory(targetObj.getType()) + File.separator
@@ -44,43 +52,49 @@ public class ParseObjectProcessor {
         cmdArray[2] = Settings.getInstance().getSymbolFileDirectory() + File.separator
                 + targetObj.getVersion() + File.separator + targetObj.getOS() + File.separator + "symbols";
         try {
+            stacktraceFile = File.createTempFile("flyte", "stacktrace");
             minidump_stacktwalk = Runtime.getRuntime().exec(cmdArray);
 
             if (minidump_stacktwalk.exitValue() == 0) {
                 stacktraceReader = new BufferedReader(new InputStreamReader(minidump_stacktwalk.getInputStream()));
-
+                stacktraceWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(stacktraceFile)));
+                
                 String temp;
 
                 do {
                     temp = stacktraceReader.readLine();
                     if (temp != null) {
-                        stacktrace += temp;
+                        stacktraceWriter.write(temp);
                     }
                 } while (temp != null);
 
                 stacktraceReader.close();
+                stacktraceWriter.close();
             }
         } catch (IOException ex) {
-            stacktrace = null;
+            stacktraceFile = null;
         }
-        return stacktrace;
+        return stacktraceFile;
     }
 
-    private static String getDescription(ParseObject targetObj) {
-        String description = "";
+    private static File getDescription(ParseObject targetObj) {
+        File description;
         String filePath = Settings.getInstance().getParseFileDownloadDirectory(targetObj.getType()) + File.separator +
                 targetObj.getName();
         
         ZipInputStream fileZIS;
+        BufferedWriter descriptionWriter;
         ZipEntry entry;
         try{
+            description = File.createTempFile("flyte", "desc");
+            descriptionWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(description)));
             fileZIS = new ZipInputStream(new FileInputStream(filePath));
             while((entry = fileZIS.getNextEntry()) != null) {
                 if(entry.getName().equals("description.txt")){
                     BufferedReader br = new BufferedReader(new InputStreamReader(fileZIS));
                     String temp;
                     while((temp = br.readLine()) != null) {
-                        description += temp;
+                        descriptionWriter.write(temp);
                     }
                 }
             }
@@ -93,6 +107,8 @@ public class ParseObjectProcessor {
         return description;
     }
 
-    private static String[] extractBugInfo(ParseObject targetObj) {
+    private static File[] extractBugInfo(ParseObject targetObj) {
+        File[] outputFiles = new File[3];
+        return outputFiles;
     }
 }
